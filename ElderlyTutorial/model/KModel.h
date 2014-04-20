a7900f6a9a700465153adfb355171a58cffd0a82
zhangwenmin <army@zhangwenmindeMacBook-Pro.local>
2014-04-20 20:35:38 +0800

    1.添加模型

---
+++ ElderlyTutorial/model/KModel.h
@@ -0,0 +1,13 @@
+//
+//  KModel.h
+//  ElderlyTutorial
+//
+//  Created by zhangwenmin on 14-4-20.
+//  Copyright (c) 2014年 LeafShadow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KModel : NSObject
+
+@end
---
+++ ElderlyTutorial/model/KModel.m
@@ -0,0 +1,13 @@
+//
+//  KModel.m
+//  ElderlyTutorial
+//
+//  Created by zhangwenmin on 14-4-20.
+//  Copyright (c) 2014年 LeafShadow. All rights reserved.
+//
+
+#import "KModel.h"
+
+@implementation KModel
+
+@end
